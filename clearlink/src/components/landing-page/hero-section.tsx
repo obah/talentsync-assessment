7c38d3fd7611a2b3dddd734846dff552b92473b1
Obaloluwa Olusoji <olusojiobah@gmail.com>
2024-01-24 00:00:42 +0100

    style: added styling to hero section

--- clearlink/src/components/landing-page/hero-section.tsx
+++ clearlink/src/components/landing-page/hero-section.tsx
@@ -2,22 +2,26 @@ import Avatars from "../../assets/images/Avatars.svg";
 import { Icons } from "../icons";
 import Partners from "../../assets/images/partners.svg";
 import ProductDemo from "../../assets/images/product-demo.svg";
+import "../styles/hero-section.css";
 
 function HeroSection() {
   return (
-    <section>
-      <div>
+    <section className="hero-section">
+      <div className="hero-section-main">
         <div>
-          <div>
-            <h1>Uniting the world, one video call at a time</h1>
-            <p>
+          <div className="hero-main-text">
+            <h1>
+              Uniting the world,
+              <br /> one video call at a time
+            </h1>
+            <p className="hero-paragraph-1">
               Experience the future of communication with ClearLink – where
               crystal-clear video conferencing meets unparalleled simplicity.
             </p>
           </div>
 
-          <div>
-            <button>Start your free trial</button>
+          <div className="hero-cta">
+            <button className="secondary-btn">Start your free trial</button>
             <a>
               <span>
                 <Icons.AiAssistant />
@@ -26,15 +30,17 @@ function HeroSection() {
             </a>
           </div>
 
-          <div>
+          <div className="hero-testimonials">
             <img src={Avatars} alt="customer images" />
             <div>
-              <div>
-                <Icons.Star />
-                <Icons.Star />
-                <Icons.Star />
-                <Icons.Star />
-                <Icons.Star />
+              <div className="star-rating">
+                <div>
+                  <Icons.Star />
+                  <Icons.Star />
+                  <Icons.Star />
+                  <Icons.Star />
+                  <Icons.Star />
+                </div>
                 <span>5.0</span>
               </div>
               <p>from 3,000+ reviews</p>
@@ -42,10 +48,10 @@ function HeroSection() {
           </div>
         </div>
 
-        <img src={ProductDemo} alt="product demo" />
+        <img src={ProductDemo} alt="product demo" className="hero-image" />
       </div>
 
-      <div>
+      <div className="hero-section-partners">
         <p>
           Join 1,500+ companies already video conferencing the ClearLink way
         </p>
