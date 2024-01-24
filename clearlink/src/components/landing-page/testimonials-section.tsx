33d2e07ddeb5d26a983a585d7e07d574aa4f016b
Obaloluwa Olusoji <olusojiobah@gmail.com>
2024-01-24 07:39:10 +0100

    style: added styling for testimonals section

--- clearlink/src/components/landing-page/testimonials-section.tsx
+++ clearlink/src/components/landing-page/testimonials-section.tsx
@@ -2,39 +2,42 @@ import { Icons } from "../icons";
 import PeopleWorking from "../../assets/images/people-working.svg";
 import ProfilePic from "../../assets/images/profile-pic.svg";
 import Shopify from "../../assets/images/shopify.svg";
+import "../styles/testimonials-section.css";
 
 function TestimonialsSection() {
   const customerName = "Sarah Thompson";
   const customerRole = "Project Manager, Shopify";
 
   return (
-    <section>
-      <div>
-        <img src={Shopify} alt="shopify icon" />
-        <div>
+    <section className="testimonials-section">
+      <div className="testimonials-details">
+        <img src={Shopify} alt="shopify icon" className="shopify-logo" />
+        <div className="star-rating">
           <Icons.Star />
           <Icons.Star />
           <Icons.Star />
           <Icons.Star />
           <Icons.Star />
         </div>
-        <p>
+
+        <p className="paragraph-2">
           ClearLink has upgraded our remote meetings. High-quality video, screen
           sharing, and top-notch security make it essential for our team.
         </p>
-        <div>
-          <div>
+
+        <div className="testimonial-switch">
+          <div className="testimonial-profile">
             <img src={ProfilePic} alt="testimony profile picture" />
             <div>
-              <h3>{customerName}</h3>
+              <p>{customerName}</p>
               <p>{customerRole}</p>
             </div>
           </div>
-          <div>
-            <button>
+          <div className="testimonial-switch">
+            <button className="icon-btn">
               <Icons.LeftIcon />
             </button>
-            <button>
+            <button className="icon-btn">
               <Icons.RightIcon />
             </button>
           </div>
